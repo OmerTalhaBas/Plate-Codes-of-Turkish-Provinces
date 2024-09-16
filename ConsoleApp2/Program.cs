c94920dcc4f97c1f6dfa31c69adeb1b1529cafa3
OmerTalhaBas <omertalhabas58@gmail.com>
2024-09-16 16:04:16 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,259 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Basla:
+            Console.WriteLine("Bir Şehir İsimi Giriniz : ");
+            string deger = Console.ReadLine().ToLower();
+            switch (deger)
+            {
+                case "adana":
+                    Console.WriteLine("Adana'nın Plaka Kodu = 01");
+                    break;
+                case "adıyaman":
+                    Console.WriteLine("Adıyama'nın Plaka Kodu = 02");
+                    break;
+                case "afyon":
+                    Console.WriteLine("Afyon'un Plaka Kodu = 03");
+                    break;
+                case "ağrı":
+                    Console.WriteLine("Ağrı'nın Plaka Kodu = 04");
+                    break;
+                case "amasya":
+                    Console.WriteLine("Amasya'nın Plaka Kodu = 05");
+                    break;
+                case "ankara":
+                    Console.WriteLine("Ankara'nın Plaka Kodu = 06");
+                    break;
+                case "antalya":
+                    Console.WriteLine("Antalya'nın Plaka Kodu = 07");
+                    break;
+                case "artvin":
+                    Console.WriteLine("Artvin'in Plaka Kodu = 08");
+                    break;
+                case "aydın":
+                    Console.WriteLine("Aydın'ın Plaka Kodu = 09");
+                    break;
+                case "balıkesir":
+                    Console.WriteLine("Balıkesir'in Plaka Kodu = 10");
+                    break;
+                case "bilecik":
+                    Console.WriteLine("Bilecik'in Plaka Kodu = 11");
+                    break;
+                case "bingöl":
+                    Console.WriteLine("Bingöl'ün Plaka Kodu = 12");
+                    break;
+                case "bitlis":
+                    Console.WriteLine("Bitlis'in Plaka Kodu = 13");
+                    break;
+                case "bolu":
+                    Console.WriteLine("Bolu'nun Plaka Kodu = 14");
+                    break;
+                case "burdur":
+                    Console.WriteLine("Burdur'un Plaka Kodu = 15");
+                    break;
+                case "bursa":
+                    Console.WriteLine("Bursa'nın Plaka Kodu = 16");
+                    break;
+                case "çanakkale":
+                    Console.WriteLine("Çanakkale'nin Plaka Kodu = 17");
+                    break;
+                case "çankırı":
+                    Console.WriteLine("Çankırı'nın Plaka Kodu = 18");
+                    break;
+                case "çorum":
+                    Console.WriteLine("Çorum'un Plaka Kodu = 19");
+                    break;
+                case "denizli":
+                    Console.WriteLine("Denizli'nin Plaka Kodu = 20");
+                    break;
+                case "diyarbakır":
+                    Console.WriteLine("Diyarbakır'ın Plaka Kodu = 21");
+                    break;
+                case "edirne":
+                    Console.WriteLine("Edirne'nin Plaka Kodu = 22");
+                    break;
+                case "elazığ":
+                    Console.WriteLine("Elazığ'nın Plaka Kodu = 23");
+                    break;
+                case "erzincan":
+                    Console.WriteLine("Erzincan'ın Plaka Kodu = 24");
+                    break;
+                case "erzurum":
+                    Console.WriteLine("Erzurum'un Plaka Kodu = 25");
+                    break;
+                case "eskişehir":
+                    Console.WriteLine("Eskişehir'in Plaka Kodu = 26");
+                    break;
+                case "gaziantep":
+                    Console.WriteLine("Gaziantep'in Plaka Kodu = 27");
+                    break;
+                case "giresun":
+                    Console.WriteLine("Giresun'un Plaka Kodu = 28");
+                    break;
+                case "gümüşhane":
+                    Console.WriteLine("Gümüşhane'nin Plaka Kodu = 29");
+                    break;
+                case "hakkari":
+                    Console.WriteLine("Hakkari'nin Plaka Kodu = 30");
+                    break;
+                case "hatay":
+                    Console.WriteLine("Hatay'ın Plaka Kodu = 31");
+                    break;
+                case "ısparta":
+                    Console.WriteLine("Isparta'nın Plaka Kodu = 32");
+                    break;
+                case "mersin":
+                    Console.WriteLine("Mersin'in Plaka Kodu = 33");
+                    break;
+                case "istanbul":
+                    Console.WriteLine("İstanbul'un Plaka Kodu = 34");
+                    break;
+                case "izmir":
+                    Console.WriteLine("İzmir'in Plaka Kodu = 35");
+                    break;
+                case "kars":
+                    Console.WriteLine("Kars'ın Plaka Kodu = 36");
+                    break;
+                case "kastamonu":
+                    Console.WriteLine("Kastamonu'nun Plaka Kodu = 37");
+                    break;
+                case "kayseri":
+                    Console.WriteLine("Kayseri'nin Plaka Kodu = 38");
+                    break;
+                case "kırklareli":
+                    Console.WriteLine("Kırklareli'nin Plaka Kodu = 39");
+                    break;
+                case "kırşehir":
+                    Console.WriteLine("Kırşehir'in Plaka Kodu = 40");
+                    break;
+                case "kocaeli":
+                    Console.WriteLine("Kocaeli'nin Plaka Kodu = 41");
+                    break;
+                case "konya":
+                    Console.WriteLine("Konya'nın Plaka Kodu = 42");
+                    break;
+                case "kütahya":
+                    Console.WriteLine("Kütahya'nın Plaka Kodu = 43");
+                    break;
+                case "malatya":
+                    Console.WriteLine("Malatya'nın Plaka Kodu = 44");
+                    break;
+                case "manisa":
+                    Console.WriteLine("Manisa'nın Plaka Kodu = 45");
+                    break;
+                case "kahramanmaraş":
+                    Console.WriteLine("Kahramanmaraş'ın Plaka Kodu = 46");
+                    break;
+                case "mardin":
+                    Console.WriteLine("Mardin'in Plaka Kodu = 47");
+                    break;
+                case "muğla":
+                    Console.WriteLine("Muğla'nın Plaka Kodu = 48");
+                    break;
+                case "Muş":
+                    Console.WriteLine("Muş'un Plaka Kodu = 49");
+                    break;
+                case "nevşehir":
+                    Console.WriteLine("Nevşehir'in Plaka Kodu = 50");
+                    break;
+                case "niğde":
+                    Console.WriteLine("Niğde'nin Plaka Kodu = 51");
+                    break;
+                case "ordu":
+                    Console.WriteLine("Ordu'nun Plaka Kodu = 52");
+                    break;
+                case "rize":
+                    Console.WriteLine("Rize'nin Plaka Kodu = 53");
+                    break;
+                case "sakarya":
+                    Console.WriteLine("Sakarya'nın Plaka Kodu = 54");
+                    break;
+                case "samsun":
+                    Console.WriteLine("Samsun'un Plaka Kodu = 55");
+                    break;
+                case "siirt":
+                    Console.WriteLine("Siirt'in Plaka Kodu = 56");
+                    break;
+                case "sinop":
+                    Console.WriteLine("Sinop'un Plaka Kodu = 57");
+                    break;
+                case "sivas":
+                    Console.WriteLine("Kangallar'ın Plaka Kodu = 58");
+                    break;
+                case "tekirdağ":
+                    Console.WriteLine("Tekirdağ'ın Plaka Kodu = 59");
+                    break;
+                case "tokat":
+                    Console.WriteLine("Tokat'ın Plaka Kodu = 60");
+                    break;
+                case "trabzon":
+                    Console.WriteLine("Trabzon'un Plaka Kodu = 61");
+                    break;
+                case "tunceli":
+                    Console.WriteLine("Tunceli'nin Plaka Kodu = 62");
+                    break;
+                case "şanlıurfa":
+                    Console.WriteLine("Şanlıurfa'nın Plaka Kodu = 63");
+                    break;
+                case "uşak":
+                    Console.WriteLine("Uşak'ın Plaka Kodu = 64");
+                    break;
+                case "van":
+                    Console.WriteLine("Van'ın Plaka Kodu = 65");
+                    break;
+                case "yozgat":
+                    Console.WriteLine("Yozgat'ın Plaka Kodu = 66");
+                    break;
+                case "zonguldak":
+                    Console.WriteLine("Zonguldak'ın Plaka Kodu = 67");
+                    break;
+                case "aksaray":
+                    Console.WriteLine("Aksaray'ın Plaka Kodu = 68");
+                    break;
+                case "bayburt":
+                    Console.WriteLine("Bayburt'un Plaka Kodu = 69");
+                    break;
+                case "karaman":
+                    Console.WriteLine("Karaman'ın Plaka Kodu = 70");
+                    break;
+                case "kırkkale":
+                    Console.WriteLine("Kırkkale'nin Plaka Kodu = 71");
+                    break;
+                case "batman":
+                    Console.WriteLine("Batman'ın Plaka Kodu = 72");
+                    break;
+                case "şırnak":
+                    Console.WriteLine("Şırnak'ın Plaka Kodu = 73");
+                    break;
+                case "bartın":
+                    Console.WriteLine("Bartın'ın Plaka Kodu = 74");
+                    break;
+                case "ardahan":
+                    Console.WriteLine("Ardahan'ın Plaka Kodu = 75");
+                    break;
+                case "ığdır":
+                    Console.WriteLine("Iğdır'ın Plaka Kodu = 76");
+                    break;
+                case "yalova":
+                    Console.WriteLine("Yalova'nın Plaka Kodu = 77");
+                    break;
+                case "karabük":
+                    Console.WriteLine("Karabük'ün Plaka Kodu = 78");
+                    break;
+                case "kilis":
+                    Console.WriteLine("Kilis'in Plaka Kodu = 79");
+                    break;
+                case "osmaniye":
+                    Console.WriteLine("Osmaniye'nin Plaka Kodu = 80");
+                    break;
+                case "düzce":
+                    Console.WriteLine("Düzce'nin Plaka Kodu = 81");
+                    break;
+            }
+            goto Basla;
+        }
+    }
+}
